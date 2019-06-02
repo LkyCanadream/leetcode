e0cbbee9e947b6c77d1464387028d3391541e740
lky <18222954740@163.com>
2019-06-02 16:01:43 +0800

    two-sum

---
+++ Algorithms/1Two Sum/two-sum.js
@@ -0,0 +1,18 @@
+//Author : Lky
+//Date : 2019-06-02
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var hash = [];
+    for(var i = 0;i < nums.length;i++){
+        var temp = target - nums[i];
+        if(hash[temp] !== undefined)
+            return [hash[temp],i];
+        hash[nums[i]] = i;
+    }
+    
+};
